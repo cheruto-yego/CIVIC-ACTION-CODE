c18bd69b978e5784a3671fcee6554b64055404e4
Cheruto_yego <107578924+cheruto-yego@users.noreply.github.com>
2023-10-18 21:53:20 +0300

    add message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/" , (req, res) => {
-    res.send("Hello World")
+    res.send("Hello World from Civic Action")
 })
 
 app.listen(3000, () => console.log("Server is running on port 3000"))
