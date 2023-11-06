b139173cc4222a8bc9a885d684c36eaef3868994
Cheruto_yego <107578924+cheruto-yego@users.noreply.github.com>
2023-11-06 12:01:03 +0300

    changes

--- index.js
+++ index.js
@@ -1,54 +1,50 @@
 const express = require('express');
-const mysql = require('mysql');
 const bodyParser = require('body-parser');
-
 const app = express();
+
+// Middleware to parse incoming request data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'law_app_db'
-});
+// Array to store user data (in-memory storage, replace it with a database in a real application)
+const users = [];
 
-connection.connect(err => {
-    if (err) {
-        console.error('Error connecting to MySQL database: ' + err.stack);
-        return;
-    }
-    console.log('Connected to MySQL database as id ' + connection.threadId);
-});
-
+// Serve the signup form
 app.get('/signup', (req, res) => {
     res.send(`
         <form action="/signup" method="post">
             Email: <input type="email" name="email" required><br>
             Username: <input type="text" name="username" required><br>
-            client_number: <input type="text" name="client_number" required><br>
+            Phone Number: <input type="tel" name="phoneNumber" required><br>
             Password: <input type="password" name="password" required><br>
             <input type="submit" value="Sign Up">
         </form>
     `);
 });
 
+// Handle form submission
 app.post('/signup', (req, res) => {
-    const { email, username, client_number, password } = req.body;
+    const { email, username, phoneNumber, password } = req.body;
+    
+    // Check if the email is already registered
+    const existingUser = users.find(user => user.email === email);
+    if (existingUser) {
+        return res.send('Email already registered. Please use a different email.');
+    }
 
-    // Insert user data into MySQL database
-    const query = 'INSERT INTO users (email, username, client_number, password) VALUES (?, ?, ?, ?)';
-    connection.query(query, [email, username, client_number, password], (err, results) => {
-        if (err) {
-            console.error('Error inserting user data: ' + err.stack);
-            return res.send('Error occurred during signup.');
-        }
-        console.log('New user registered with ID: ' + results.insertId);
-        res.send('Signup successful!');
-    });
+    // Store the user data (in-memory storage, replace it with database storage in a real application)
+    const newUser = { email, username, phoneNumber, password };
+    users.push(newUser);
+
+    // For demonstration purposes, just print the user data
+    console.log('New user registered:', newUser);
+    res.send('Signup successful!');
+
+    // Redirect to a thank you page or login page after successful signup
+    // res.redirect('/thankyou');
 });
 
-
+// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
---
+++ signup.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'law_app_db'
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Error connecting to MySQL database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to MySQL database as id ' + connection.threadId);
+});
+
+app.get('/signup', (req, res) => {
+    res.send(`
+        <form action="/signup" method="post">
+            Email: <input type="email" name="email" required><br>
+            Username: <input type="text" name="username" required><br>
+            client_number: <input type="text" name="client_number" required><br>
+            Password: <input type="password" name="password" required><br>
+            <input type="submit" value="Sign Up">
+        </form>
+    `);
+});
+
+app.post('/signup', (req, res) => {
+    const { email, username, client_number, password } = req.body;
+
+    // Insert user data into MySQL database
+    const query = 'INSERT INTO users (email, username, client_number, password) VALUES (?, ?, ?, ?)';
+    connection.query(query, [email, username, client_number, password], (err, results) => {
+        if (err) {
+            console.error('Error inserting user data: ' + err.stack);
+            return res.send('Error occurred during signup.');
+        }
+        console.log('New user registered with ID: ' + results.insertId);
+        res.send('Signup successful!');
+    });
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
